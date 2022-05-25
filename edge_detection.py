02a4c28d87b17ccf2d5b4623655b17aed686def0
WJ_Fifth <wjw104655073@outlook.com>
2022-05-25 13:21:16 +1000

    The project code

--- edge_detection.py
+++ edge_detection.py
@@ -4,8 +4,9 @@ from PIL import Image
 import math
 
 
-def Sobel(path, save_path, number):
-    path = path + "%d_src.png" % number
+def Edge_detection(path, save_path, number):
+    path = path + "%d.png" % number
+    print(path)
     img = cv2.imread(path)
 
     Gx = cv2.Sobel(img, cv2.CV_16S, 1, 0)
@@ -16,11 +17,11 @@ def Sobel(path, save_path, number):
 
     # Tend = math.atan2(absY, absX)
 
-    dst = cv2.addWeighted(absX, 0.5, absY, 0.5, 0)  # Fusion of two images
-    save_path = save_path + "%d_edge.png" % number
-    cv2.imwrite(save_path, dst)
+    # dst = cv2.addWeighted(absX, 0.5, absY, 0.5, 0)  # Fusion of two images
+    # save_path = save_path + "%d_edge.png" % number
+    # cv2.imwrite(save_path, dst)
 
-    edge = cv2.Canny(dst, 50, 100)
+    # edge = cv2.Canny(dst, 50, 100)
     # cv2.imshow("Canny_edge_1", edge)
     edge1 = cv2.Canny(Gx, Gy, 10, 100)
     # cv2.imshow("Canny_edge_2", edge1)
@@ -31,10 +32,11 @@ def Sobel(path, save_path, number):
     cv2.imwrite(save_path1, edge2)
 
 
-for i in range(30):
-    # path = "D:/VOC2012/JPEGImages/"
-    # save_path = "D:/VOC2012/JPEGImages/EdgeDetection/"
-    path = "E:/FCN_Code/fcn_pytorch/data/result/"
-    save_path = "E:/FCN_Code/fcn_pytorch/data/result/edge/"
-    Sobel(path, save_path, i)
-print("Edge extraction completed")
+if __name__ == "__main__":
+    for i in range(1, 21):
+        # path = "D:/VOC2012/JPEGImages/"
+        # save_path = "D:/VOC2012/JPEGImages/EdgeDetection/"
+        path = "E:/FCN_Code/data/detection/"
+        save_path = "E:/FCN_Code/data/detection/edge/"
+        Edge_detection(path, save_path, i)
+    print("Edge extraction completed")
